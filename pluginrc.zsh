ed728ff9faf6a82688d398127c53c14a138d2b23
nom05 <70644469+nom05@users.noreply.github.com>
2023-01-28 01:25:56 +0100

    Update pluginrc.zsh

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -2,3 +2,7 @@
 # Put here your functions, environment variables, aliases and whatever you need.
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+plugin_name='xxh-plugin-zsh-fzf'
+
+export PATH=$CURR_DIR/bin:$PATH
+#fpath=($CURR_DIR/completions $fpath)
