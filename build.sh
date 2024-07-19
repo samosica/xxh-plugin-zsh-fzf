291a923e708f0a70c91ec4e2b6877e9a566798b1
samosica <samosic4@gmail.com>
2024-07-19 16:42:09 +0900

    Rewrite

--- build.sh
+++ build.sh
@@ -1,46 +1,50 @@
 #!/usr/bin/env bash
+set -eu
 
 CDIR="$(cd "$(dirname "$0")" && pwd)"
-build_dir=$CDIR/build
+readonly CDIR
+readonly build_dir=$CDIR/build
 FZF_VERSION=$(cat fzf-version)
 readonly FZF_VERSION
 
-while getopts A:K:q option
-do
-  case "${option}"
-  in
-    q) QUIET=1;;
-    A) ARCH=${OPTARG};;
-    K) KERNEL=${OPTARG};;
-  esac
+QUIET=''
+
+while getopts A:K:q option; do
+    case "${option}" in
+        q) QUIET=1;;
+        A) # shellcheck disable=SC2034
+           ARCH=${OPTARG};;
+        K) # shellcheck disable=SC2034
+           KERNEL=${OPTARG};;
+        *) ;;
+    esac
 done
 
-rm -rf $build_dir
-mkdir -p $build_dir
+rm -rf "$build_dir"
+mkdir -p "$build_dir"
+cp "$CDIR/pluginrc.zsh" "$build_dir/"
 
-for f in pluginrc.zsh
-do
-    cp $CDIR/$f $build_dir/
-done
+readonly portable_url="https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz"
+tarname=$(basename "$portable_url")
+readonly tarname
 
-portable_url="https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz"
-tarname=`basename $portable_url`
-
-cd $build_dir
+cd "$build_dir"
 
 [ $QUIET ] && arg_q='-q' || arg_q=''
 [ $QUIET ] && arg_s='-s' || arg_s=''
 [ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
 
 if [ -x "$(command -v wget)" ]; then
-  wget $arg_q $arg_progress $portable_url -O $tarname
+    # shellcheck disable=SC2086
+    wget $arg_q $arg_progress "$portable_url" -O "$tarname"
 elif [ -x "$(command -v curl)" ]; then
-  curl $arg_s -L $portable_url -o $tarname
+    # shellcheck disable=SC2086
+    curl $arg_s -L "$portable_url" -o "$tarname"
 else
-  echo Install wget or curl
+    echo Install wget or curl
 fi
 
-tar -xzf $tarname
+tar -xzf "$tarname"
 mkdir bin
-mv fzf bin
-rm $tarname
+mv fzf bin/
+rm "$tarname"
